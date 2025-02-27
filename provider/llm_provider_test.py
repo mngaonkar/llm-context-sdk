a9189bc2c44e7c5a294c849192c8551bb67101aa
Mahadev Gaonkar <mahadevgaonkar@Mahadevs-MacBook-Pro.local>
2025-02-27 01:54:47 -0800

    added fancy markdown display

--- provider/llm_provider_test.py
+++ provider/llm_provider_test.py
@@ -1,6 +1,8 @@
 import unittest
 from llm_provider import LLMOllama
 from loguru import logger
+from rich.markdown import Markdown
+from rich.console import Console
 
 class TestLLMProvider(unittest.TestCase):
     def setUp(self):
@@ -11,8 +13,9 @@ class TestLLMProvider(unittest.TestCase):
         self.provider.get_models_list()
 
     def test_generate_response(self):
+        console = Console()
         response = self.provider.generate_response("Best places to visit in california?")
-        logger.info(response)
+        console.print(Markdown(response))
 
 if __name__ == "__main__":
     unittest.main()
