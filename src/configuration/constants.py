8ebb3914085c280a3ae1247a2c2bad360d3dbe33
mahadev gaonkar <mahadev.gaonkar@gmail.com>
2025-11-30 02:19:04 -0800

    config db fixed

--- src/configuration/constants.py
+++ src/configuration/constants.py
@@ -1,6 +1,7 @@
 # Setup config DB. TODO: refactor, read from file and configure
 CONFIG_FILES_PATH = "./deploy/configuration"
 
-CONFIG_DB_PATH = "/app/deploy/configdb"
+CONFIG_DB_PATH = "./deploy/configdb"
 CONFIG_DB_NAME = "config.db"
+CONFIG_DB = f"{CONFIG_DB_PATH}/{CONFIG_DB_NAME}"
 
